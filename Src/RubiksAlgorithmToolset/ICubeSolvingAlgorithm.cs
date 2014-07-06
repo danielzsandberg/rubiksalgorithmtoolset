60151d7b0524d8fffe70dcc814be555b37490994
Daniel Sandberg <dsandber@andrew.cmu.edu>
2014-07-06 01:51:56 -0400

    Initial commit

---
+++ Src/RubiksAlgorithmToolset/ICubeSolvingAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using RubiksCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksApp.RubiksAlgorithmToolset
+{
+    public interface ICubeSolvingAlgorithm
+    {
+        string AlgorithmName { get; }
+        string Author { get; }
+        string Description { get; }
+        void Solve(RubiksCube cube);
+    }
+}
